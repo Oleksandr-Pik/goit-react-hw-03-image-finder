ba14efba65fefec4510b83cd49625388d773f428
Oleksandr-Pik <pik.aleksandr79@gmail.com>
2023-12-11 01:38:51 +0200

    2

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -3,7 +3,7 @@ import Modal from 'components/Modal';
 
 class ImageGalleryItem extends Component {
   state = {
-    // showModal: false,
+    showModal: false,
   };
 
   toggleModal = () => {
@@ -12,29 +12,29 @@ class ImageGalleryItem extends Component {
     }));
   };
 
-  // handleClick = () => {
-  //   console.log('click ');
-  //   this.state.toggleModal();
-  // };
+  handleClick = () => {
+    console.log('click ');
+    this.state.toggleModal();
+  };
 
   render() {
     return (
       <>
         {this.props.images.map(({ id, tags, webformatURL, largeImageURL }) => (
           <li className="ImageGalleryItem" key={id}>
-            <a className='' href={largeImageURL} onClick={this.toggleModal}>
+            {/* <a className='' href={largeImageURL} onClick={this.toggleModal}> */}
             <img
               className="ImageGalleryItem-image"
               src={webformatURL}
               alt={tags}
               onClick={this.toggleModal}
             />
-            </a>
-        {/* {this.state.showModal && <Modal 
+            {/* </a> */}
+        {this.state.showModal && <Modal 
         onClose={this.toggleModal} 
-        // largeImageURL={largeImageURL} 
-        // tags={tags}
-        />} */}
+        largeImageURL={largeImageURL} 
+        tags={tags}
+        />}
           </li>
         ))}
       </>
